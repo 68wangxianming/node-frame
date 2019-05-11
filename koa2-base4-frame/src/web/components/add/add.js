7fef8cb6de0dad13db2f932131332ad9e28384a6
wangxianming <1842344641@qq.com>
2019-05-11 16:34:39 +0800

    frame4

---
+++ koa2-base4-frame/src/web/components/add/add.js
@@ -0,0 +1,9 @@
+import './add.css'
+
+const add = {
+    init() {
+        console.log('init add.html🍎');
+    }
+}
+
+export default add;
---
+++ koa2-base4-frame/src/web/components/list/list.js
@@ -0,0 +1,9 @@
+import './list.css'
+
+const list = {
+    init() {
+        console.log('init list.html🍎');
+    }
+}
+
+export default list;
